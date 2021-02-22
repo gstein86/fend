67c3d70a7371009348b8abcff6bc500aff3a9acd
Georg Stein <gstein@buhl-data.com>
2021-02-22 16:34:54 +0100

    initial commit

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -2,18 +2,45 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8080;
+const server = app.listen(port, listening);
+function listening() {
+    console.log(`running on localhost: ${port}`);
+};
+
+// GET route
+app.get('/all', sendData);
+
+function sendData (request, response) {
+  response.send(projectData);
+};
+
+// POST route
+app.post('/postData', addData);
+
+function addData(request, response) {
+    projectData.date = request.body.date;
+    projectData.temp = request.body.temp;
+    projectData.content = request.body.content;
+
+    response.send(projectData);
+}
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,72 @@
 /* Global Variables */
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const key = '48819b20cc01b2c872ac05750d77cb1b';
+const localhost = 'http://localhost:8080';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// add click event listener to button
+document.getElementById('generate').addEventListener('click', generateEntry);
+
+// button click callback function, which uses user input and chains async calls to finally update the user interface
+function generateEntry(e) {
+    const zip = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+
+    getTemperature(zip)
+        .then(function (data) {
+            postData({ date: newDate, temp: data.main.temp, content: feelings })
+                .then(function () {
+                    updateUI()
+                })
+        });
+}
+
+// async function to fetch data from weather api for input zip code
+const getTemperature = async (zip) => {
+    const response = await fetch(baseUrl + '?zip=' + zip + '&appid=' + key);
+
+    try {
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.log('error', error);
+    }
+}
+
+// async function to post data to local server
+const postData = async (data = {}) => {
+    const response = await fetch(localhost + '/postData', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    } catch (error) {
+        console.log('error', error);
+    }
+}
+
+// async function to fetch data from local server and update ui
+const updateUI = async () => {
+    const response = await fetch(localhost + '/all');
+
+    try {
+        const data = await response.json();
+        document.getElementById('date').innerHTML = 'date: ' + data.date;
+        document.getElementById('temp').innerHTML = 'temperature: ' + data.temp;
+        document.getElementById('content').innerHTML = 'feelings: ' + data.content;
+    } catch (error) {
+        console.log('error', error);
+    }
+}
